30cf1abfbc5193cbf8e6cd497e4f68b5e689f6c8
Yuang <yuang.tong@outlook.com>
2024-04-08 01:36:32 -0500

    template added

--- exParcial/tareasApp/views.py
+++ exParcial/tareasApp/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Bienvenidos a tareasApp")
+
+def welcome(request):
+    return render(request, "welcome.html")
